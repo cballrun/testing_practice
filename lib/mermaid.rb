57db3c55001862404228d22d1b7937bd8283d9f9
Carter Ball <carter.k.ball@gmail.com>
2022-06-15 15:03:52 -0400

    Create get_older method and test Mermaid class

--- lib/mermaid.rb
+++ lib/mermaid.rb
@@ -2,7 +2,7 @@ class Mermaid
   attr_reader :name
   attr_reader :age
 
-  def initialize
+  def initialize(name)
     @name = name
     @age = 22
   end
@@ -10,4 +10,11 @@ class Mermaid
   def get_older
     @age = @age + 1
   end
+
 end
+
+m1 = Mermaid.new("Sereia")
+
+m1.get_older
+
+p m1
